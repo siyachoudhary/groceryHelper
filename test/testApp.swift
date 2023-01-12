c6fcb737bf372fae419a0eb472b88e690e6dd3d3
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2023-01-12 17:04:12 -0600

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Siya Choudhary on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
